0da4951c9cd91974cccc323ed1999a129d73ac38
raroepke <48627923+raroepke@users.noreply.github.com>
2020-11-08 23:53:08 -0500

    Update viewer-service.php

--- viewer-service.php
+++ viewer-service.php
@@ -1,7 +1,7 @@
 <?php
 // PHP Data Objects(PDO) Sample Code:
 try {
-    $conn = new PDO("sqlsrv:server = tcp:gradesdbserver.database.windows.net,1433; Database = gradesdatabase", "raroepke", "{your_password_here}");
+    $conn = new PDO("sqlsrv:server = tcp:gradesdbserver.database.windows.net,1433; Database = gradesdatabase", "raroepke", "hellohello_22");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
 catch (PDOException $e) {
