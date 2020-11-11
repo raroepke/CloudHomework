f241784b3312143c9f33cbe61ee4e60a1ab718f1
raroepke <48627923+raroepke@users.noreply.github.com>
2020-11-11 15:37:46 -0500

    Create test-connection.php

---
+++ test-connection.php
@@ -0,0 +1,11 @@
+<?php
+// PHP Data Objects(PDO) Sample Code:
+try {
+    $conn = new PDO("sqlsrv:server = tcp:gradesdbserver.database.windows.net,1433; Database = gradesdatabase", "raroepke", "hellohello_22");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
+}
+?>
